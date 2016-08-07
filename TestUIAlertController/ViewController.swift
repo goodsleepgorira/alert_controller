3df1e1695697569feb7d78b4790e388de97512be
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-08-06 21:15:36 -0400

    Test

--- TestUIAlertController/ViewController.swift
+++ TestUIAlertController/ViewController.swift
@@ -6,10 +6,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //最初からあるメソッド
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    //ボタン押下時の呼び出しメソッド
+    @IBAction func pushButton(sender: UIButton) {
+    }
 
 }
 
