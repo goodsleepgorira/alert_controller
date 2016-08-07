6c11fbe2171cb7a97cee09ff3c5c1b43221300a1
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-08-06 22:49:54 -0400

    Test

--- TestUIAlertController/ViewController.swift
+++ TestUIAlertController/ViewController.swift
@@ -6,6 +6,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     //最初からあるメソッド
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,7 +14,13 @@ class ViewController: UIViewController {
 
     //ボタン押下時の呼び出しメソッド
     @IBAction func pushButton(sender: UIButton) {
-    }
 
+        //ストリーボードにある遷移先ビューコントローラーを取得する。
+        let controller = self.storyboard!.instantiateViewControllerWithIdentifier("nextViewController")
+                    
+        //遷移先ビューコントローラーに画面遷移する。
+        self.navigationController?.pushViewController(controller, animated: true)
+
+    }
 }
 
